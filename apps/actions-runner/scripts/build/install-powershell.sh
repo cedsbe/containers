969c07d31aae24dad9327b2057ffb595a9102760
cedsbe <8707686+cedsbe@users.noreply.github.com>
2026-06-21 20:44:37 +0200

    feat(actions-runner): implement custom PowerShell installation for arm64 architecture

--- apps/actions-runner/scripts/build/install-powershell.sh
+++ apps/actions-runner/scripts/build/install-powershell.sh
@@ -10,6 +10,20 @@ source $HELPER_SCRIPTS/os.sh
 
 pwsh_version=$(get_toolset_value .pwsh.version)
 
-# Install Powershell
+# Custom installation of PowerShell for arm64 architecture, since Microsoft does not provide a deb package for it.
+# For amd64, we can use the deb package.
 
+dpkg_arch=$(dpkg --print-architecture)
+
+if [ "$dpkg_arch" = "amd64" ]; then
     apt-get install powershell=$pwsh_version*
+elif [ "$dpkg_arch" = "arm64" ]; then
+    pwsh_archive_url=$(resolve_github_release_asset_url "PowerShell/PowerShell" 'endswith("linux-arm64.tar.gz")' "${pwsh_version}" "false" "true")
+    archive_path=$(download_with_retry "$pwsh_archive_url")
+    mkdir -p /opt/microsoft/powershell/7
+    tar xzf "$archive_path" -C /opt/microsoft/powershell/7
+    chmod +x /opt/microsoft/powershell/7/pwsh
+    ln -sf /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+else
+    echo "Unsupported architecture for powershell: $dpkg_arch"; exit 1
+fi
